be703bd4bb7f8867e57ce582bd826431610cfc34
Franck Lambinet <franck.lambinet@gmail.com>
2026-06-14 13:15:44 +0200

    fix: update docusaurus config with correct github repository paths

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,15 +22,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://francklambinet-del.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/NeuroScaling/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'francklambinet-del', // Usually your GitHub org/user name.
+  projectName: 'NeuroScaling', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
